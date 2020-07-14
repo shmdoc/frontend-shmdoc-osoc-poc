a58b482c529ffa570e85f356685c0282a42952fe
thdossch <dossche.thor@gmail.com>
2020-07-14 10:15:43 +0200

    aanmaak JobCreator component

---
+++ src/components/JobCreator.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+    //<input v-model="name" placeholder="name"/>
+    //<button v-on:click="add_name">Add name</button>
+export default {
+  data() {
+    return {
+      job: null,
+    }
+  },
+  methods: {
+
+  }
+</script>
