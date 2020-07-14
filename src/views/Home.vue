c58ec895ad2f63d51183d3d57cc46ad5b710febf
thdossch <dossche.thor@gmail.com>
2020-07-14 11:02:33 +0200

    Removed Namelist from home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,6 @@
 <template>
 	<div>
 		<h1>Shmdoc</h1>
-		<NameList/>
-    <hr/>
     <FileList/>
     <hr/>
     <FileUploader/>
@@ -12,13 +10,11 @@
 </template>
 
 <script>
-import NameList from '@/components/NameList.vue'
 import FileList from '@/components/FileList.vue'
 import FileUploader from '@/components/FileUploader.vue'
 import FileDeleter from '@/components/FileDeleter.vue'
 export default {
 	components: {
-		NameList,
     FileList,
     FileUploader,
     FileDeleter
