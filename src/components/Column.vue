6256f76ae67eca440326f2651de2ebf4b6a89684
thdossch <dossche.thor@gmail.com>
2020-07-16 13:59:18 +0200

    removed commented html

--- src/components/Column.vue
+++ src/components/Column.vue
@@ -59,28 +59,6 @@
 
 <script>
 
-//      <tr>
-//        <th><++></th>
-//        <td>{{column.<++>}}</th>
-//      </tr>
-
-
-//    <h5>description:</h5>
-//    <p>{{column.description}}</p>
-//    <h5>note:</h5>
-//    <p>{{column.note}}</p>
-//    <h5>datatype: {{column.datatype}}</h5>
-//    <h5>quantity kind:{{column.quantityKind}}</h5>
-//    <h5>unit: {{column.unit}}</h5>
-//    <h5>record count: {{column.recordCount}}</h5>
-//    <h5>missing count: {{column.missingCount}}</h5>
-//    <h5>null count: {{column.nullCount}}</h5>
-//    <h5>min: {{column.min}}</h5>
-//    <h5>max: {{column.max}}</h5>
-//    <h5>mean: {{column.mean}}</h5>
-//    <h5>median: {{column.median}}</h5>
-//    <h5>common values: {{column.commonValues}}</h5>
-
 export default {
   props: ['column'],
   data() {
