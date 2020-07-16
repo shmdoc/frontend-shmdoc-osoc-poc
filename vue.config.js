498e986d3366aa6e38b2a33f0e6a9d83cb1013d4
Michaël Dierick <michael.dierick@redpencil.io>
2020-07-16 15:37:51 +0200

    Don't proxy for html-requests

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,13 @@ module.exports = {
   devServer: {
     proxy: {
       '/': {
-          target: 'http://localhost:80',
-          changeOrigin: true
-      },
+        target: 'http://localhost:80',
+        bypass: function(req) {
+          if (req.headers.accept.indexOf('html') !== -1) {
+            return '/index.html';
+          }
+        }
+      }
     }
   }
 }
