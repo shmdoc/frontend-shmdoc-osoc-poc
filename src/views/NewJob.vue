5ea9482e9cfe791975559769e6237a5bd010c768
thdossch <dossche.thor@gmail.com>
2020-07-16 14:47:10 +0200

    removed show files button on new job page

--- src/views/NewJob.vue
+++ src/views/NewJob.vue
@@ -2,19 +2,15 @@
   <div>
     <h1>New Job</h1>
     <FileUploader/>
-    <hr/>
-    <FileList/>
   </div>
 </template>
 
 <script>
 import FileUploader from '@/components/FileUploader.vue'
-import FileList from '@/components/FileList.vue'
 
 export default {
   components: {
-    FileUploader,
-    FileList
+    FileUploader
   }
 }
 </script>
