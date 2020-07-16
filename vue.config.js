c8ac6fb3cabc2bbb609149b39e7a4a89ea7537d3
thdossch <dossche.thor@gmail.com>
2020-07-16 14:27:40 +0200

    change in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,8 @@
-////config/index.js
-//module.exports = {
-//  dev: {
-//    proxyTable: {
-//  // proxy all requests starting with /api to jsonplaceholder
-//      '/': {
-//        target: 'http://localhost:80',
-//        changeOrigin: true,
-//      }
-//    }
-//  }
-//}
-
 module.exports = {
   devServer: {
     proxy: {
-      '^/': {
+      '/': {
           target: 'http://localhost:80',
-          ws: true,
           changeOrigin: true
       },
     }
