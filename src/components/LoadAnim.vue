411bc2ba75d57e8e827f62f2d93e9e7fdd5f3e57
thdossch <dossche.thor@gmail.com>
2020-07-17 14:03:16 +0200

    load animation component

---
+++ src/components/LoadAnim.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="box">
+    <transition name="load">
+      <div v-if="show" class="ball"></div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true
+    }
+  },
+  created() {
+    setInterval(() => {
+      this.show = !this.show
+    }, 500)
+  }
+}
+</script>
+
+<style scoped>
+.box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  widht: 20px;
+  height: 20px;
+}
+.ball {
+  width: 10px;
+  height: 10px;
+  background-color: black;
+  border-radius: 50%
+}
+.load-enter, .load-leave-to {
+  width: 15px;
+  height: 15px;
+}
+.load-enter-active, .load-leave-active {
+  transition: all 0.5s;
+}
+</style>
