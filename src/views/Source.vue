9b0d97f93110fc2fef283bfe48eefa8733dc05d5
arnodeceuninck <arno.deceuninck@student.uantwerpen.be>
2020-07-22 18:46:45 +0200

    Showing list of jobs per source possible to click on a job and go to the job infb

--- src/views/Source.vue
+++ src/views/Source.vue
@@ -1,14 +1,27 @@
+<!-- Note: I know I've written way too much comments, but that also helps me to understand everything -->
+
 <template>
   <div>
+
+    <!-- Page header: Info about the current source -->
     <h2>Source "{{source.attributes.name}}"</h2>
-        <h3
+    <p>{{source.attributes.description}}</p>
+    <!-- TODO: Why does the description not work? :-( -->
+
+        <!-- Clickable card per job of the current source -->
+        <div
           v-for="job in jobs"
           :key="job.id"
           class="column-card"
           v-on:click="changeSelected(job)"
         >
-            {{job.attributes.created}}
-        </h3>
+        <!-- router-link makes it clickable and redirect to the link with name "job" and the id corresponding to the current job -->
+        <router-link :to="{ name: 'job', params: {id: job.id} }" class="link">
+            <h3>Job created on {{job.attributes.created}}</h3>
+            <p>Job ID: {{job.id}}</p>
+          </router-link>
+        </div>
+
   </div>
 </template>
 
@@ -27,12 +40,13 @@ export default {
     fetch_source_jobs() {
       // A function which stores all jobs connected to the given source
       //  and stores them in jobs
-      console.log("Fetching source jobs...")
-      fetch('/sources/' + this.$route.params.id + '/analysis-jobs') // Get the analysis jobs in json format
-          .then(response => response.json())
+      var fetch_location = '/sources/' + this.$route.params.id + '/analysis-jobs'
+      // console.log("Fetching source jobs from " + fetch_location + "...")
+      fetch(fetch_location) // Get the analysis jobs in json format
+          .then(response => response.json()) // Get the json part of the response (skip headers etc)
           .then(response => {
             if (response.data.length) {
-              // If there is a valid response, add each element to the job list
+              // If there is data in the response, add each element to the job list
               response.data.forEach(job => this.jobs.push(job))
             } else {
               console.log("Response data is empty :-(")
@@ -40,11 +54,9 @@ export default {
           })
           .catch(error => console.log(error))
     },
-    changeSelected () {
-      // Navigate to job
-    },
   },
   mounted: function() {
+    // Functions that get executed when the page is loaded
     this.source = this.$store.getters.getSourceById(this.$route.params.id) // Get the current source
     this.fetch_source_jobs() // Fetch jobs for data.jobs
   }
