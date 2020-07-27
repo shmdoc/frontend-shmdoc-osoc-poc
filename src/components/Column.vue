65709acd2af197b4162394e27d96bd6edb47ca6a
Michaël Dierick <michael.dierick@redpencil.io>
2020-07-27 14:14:53 +0200

    No need to decode empty patch responses

--- src/components/Column.vue
+++ src/components/Column.vue
@@ -235,7 +235,6 @@
           headers: { 'Content-Type': 'application/vnd.api+json' },
           body: JSON.stringify(payload)
         })
-          .then(response => response.json())
           .then(response => console.log(response))
           .catch(error => console.log(error))
       },
